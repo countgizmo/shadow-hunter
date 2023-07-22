fc58f2eed883515e481dd787fa15b4753b240a2b
countgizmo <count.gizmo@gmail.com>
2023-07-22 14:49:00 +0200

    Adding UI deps
    Bubble Tea, Bubble and Lipgloss.
    Also showing a vector as a Table just to test it works.

--- main.go
+++ main.go
@@ -2,12 +2,98 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"ziggytwister.com/shadow-hunter/ast"
 	"ziggytwister.com/shadow-hunter/lexer"
+	"ziggytwister.com/shadow-hunter/parser"
 )
 
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc", "q":
+			return m, tea.Quit
+		}
+	}
+
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
 func main() {
-	input := `(){}[]`
+	input := "[:name :age 12]"
+
 	l := lexer.New(input)
-	fmt.Println(l.NextToken())
+	p := parser.New(l)
+
+	edn := p.ParseEDN()
+	columns := []table.Column{
+		{Title: "Index", Width: 4},
+		{Title: "Element", Width: 10},
+	}
+
+	rows := []table.Row{}
+
+	for _, element := range edn.Elements {
+		switch element := element.(type) {
+		case *ast.VectorElement:
+			{
+				for idx, ve := range element.Elements {
+					row := []string{strconv.Itoa(idx), ve.String()}
+					rows = append(rows, row)
+				}
+			}
+		}
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+
+	t.SetStyles(s)
+
+	m := model{t}
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
